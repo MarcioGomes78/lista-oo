736d6581fd289c5ad2308f4a0f7759eba912ac12
MarcioGomes78 <mjgomes1978@gmail.com>
2022-07-22 00:54:20 -0300

    Funcao Lambda e verificando se id ja existe

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -24,7 +24,7 @@ de aumento por porcentagem dada.*/
 public class Program {
 
 	public static void main(String[] args) {
-		//Para colocar o separador de decimais do tipo de moeda de um pais 
+		// Para colocar o separador de decimais do tipo de moeda de um pais 
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
@@ -36,12 +36,19 @@ public class Program {
 		// Dê preferência começar o for com i=o 
 		for (int i=0; i<N; i++) {
 			
-			System.out.println("***************************");
+			System.out.println();
+			System.out.println("############## **Identity Employees** ################");
 			System.out.println("Employee #" + (i + 1) + ": ");
 			System.out.print("Id: ");
 			// 1- O nextInt gero uma quebra de linha no Buffer de entrada padrão
 			Integer id = sc.nextInt();
 			
+			// Para testar se o id já existe, 
+			while (hasId(list, id)) {
+				System.out.println("ID already taken! Try again: ");
+				// se existe tente denovo
+				id = sc.nextInt();
+			}
 			System.out.print("Name: ");
 			// 3- Serve consumir o enter na entrada do nextInt que ficou pendente na quebra de linha
 			sc.nextLine();
@@ -57,19 +64,43 @@ public class Program {
 			// por padrão adiciona no fim da lista
 			list.add(emp);
 		}
-		System.out.println("########################");
+		System.out.println();
+		System.out.println("############### **Employees Incresed** ###############");
 		System.out.println("Enter the employee id that will have salary increase: ");
 		int idSalary = sc.nextInt(); 
 		Integer pos = positionId(list, idSalary);
+		
+		// *****Função Lambda para encontar o id***** 
+		// Employee emp = list.stream().filter(x -> x.getId() == idsalary).findFirst().orElse(null);
+		// if (emp == null) {
+		
+		
 		if (pos == null) {
+			System.out.println();
 			System.out.println("This id does not exist!");
 		}
 		else {
 			System.out.print("Enter the porcentage: ");
 			double percent = sc.nextDouble();
-			//Pega acessar o funcionario que esta na posição (pos) 
+			// Pega acessar o funcionario que esta na posição (pos) 
 			// Então incrementa o salario com a porcentage
 			list.get(pos).increaseSalary(percent);
+			// Usando o Lambda
+			// emp.increaseSalary(percent);
+		}
+		
+		System.out.println();
+		System.out.println("############### **Result Final** ###############");
+		System.out.println("List of employees: ");
+		// for each serve para pegar cada empregado em emp, na List<Employee> list
+		for (Employee emp : list) {
+			// Imprima o emp
+			System.out.println(emp);
+			
+			// Usando Lambda
+			// for (Employee e : list) {
+				// Imprima o e
+				//System.out.println(e);
 		}
 		
 		sc.close();
@@ -85,5 +116,17 @@ public class Program {
 			}
 		}
 		return null;
+	}
+	
+	public static boolean hasId(List<Employee> list, int id) {
+		// Função Lambda 
+		// Lógica: A função Lambda transforma sua lista num stream
+		// 1- O stream é uma função do java que aceita funções de auta ordem(Lambda)
+		// 2- O filter filtra da lista só aqueles elementos um predicado que 
+		// for colocado(x ->(tal que) x.getId == id). ->(tal que)
+		// O findFirst tenta buscar o primeiro e não existir
+		// o orElse se não existir reorna nulo
+		Employee emp = list.stream().filter(x -> x.getId() == id).findFirst().orElse(null);
+		return emp != null;
 	}
 }
--- src/entities/Employee.java
+++ src/entities/Employee.java
@@ -48,7 +48,7 @@ public class Employee {
 
 	@Override
 	public String toString() {
-		return "Employee [id=" + id + ", name=" + name + ", salary=" + salary + "]";
+		return "Employee [id: " + id + ", name: " + name + ", salary: " + salary + "]";
 	}
 	
 	
